26705b31bdcf157f390800eca0fa2a879f22f4d0
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-02-29 04:49:39 +0400

    style: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,58 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      '3xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      '2xl': { max: '1440px' },
+      // => @media (max-width: 1440px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' }
+      // => @media (max-width: 639px) { ... }
     },
+
+    extend: {
+      padding: {
+        section_x: 'var(--section-inline-padding)',
+        section_y: 'var(--section-block-padding)',
+        section_card_offset:
+          'calc(var(--section-block-padding) + var(--card-offset))'
+      },
+      margin: {
+        card_offset: 'var(--card-offset)'
+      },
+      fontSize: {
+        h1: 'var(--h1)',
+        h2: 'var(--h2)',
+        h3: 'var(--h3)',
+        h4: 'var(--h4)',
+        h5: 'var(--h5)',
+        h6: 'var(--h6)',
+        p: 'var(--p)'
+      },
+      gap: {
+        container: 'var(--container-gap)',
+        grid: 'var(--grid-gap)',
+        content: 'var(--content-gap)'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
